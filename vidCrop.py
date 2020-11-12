0974c953e52c4bc13a58872b4d98105c9a77406f
mitchellciupak <ciupak@purdue.edu>
2020-11-11 22:02:29 -0500

    TODO added

--- vidCrop.py
+++ vidCrop.py
@@ -2,6 +2,7 @@
 
 import sys
 import cv2 as cv
+import tkinter as tk
 # import gui
 
 if __name__ == '__main__':
@@ -14,8 +15,24 @@ if __name__ == '__main__':
     print('Number of arguments:', len(sys.argv), 'arguments.')
     print('Argument List:', str(sys.argv))
 
+    # TODO FINISH GUI https://www.youtube.com/watch?v=D8-snVfekto
     if (len(sys.argv) < 2):
-        print("TODO Launch GUI")
+        root = tk.Tk()
+        canvas = tk.Canvas(root, height=600, width=800)
+        canvas.pack()
+
+        frame = tk.Frame(root, bg="gray")
+        frame.place(relx=0.05, rely=0.05, relwidth=0.95, relheight=0.95)
+
+        xButton = tk.Button(frame, text="X Slider and Text Box")
+        xButton.pack()
+        yButton = tk.Button(frame, text="Y Slider and Text Box")
+        yButton.pack()
+
+        upload = tk.Button(root, text="Upload a file")
+        upload.pack()
+
+        root.mainloop()
         # GUI Collect Arguments
         # GUI feed arguments too CLI
     else:
